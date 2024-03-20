61e8ae83d43c3e04320b8adf58dbd44f418d1beb
Charles Lamb <sc21cl@leeds.ac.uk>
2024-03-20 12:39:40 +0000

    initial site created

---
+++ src/components/hero/Aurora.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import { Stars } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import React, { useEffect } from "react";
+import { FiArrowRight } from "react-icons/fi";
+import {
+  useMotionTemplate,
+  useMotionValue,
+  motion,
+  animate,
+} from "framer-motion";
+import SoftwareStack from "./SoftwareStack";
+
+const COLORS_TOP = ["#13FFAA", "#1E67C6", "#CE84CF", "#DD335C"];
+
+export const Aurora = () => {
+  const color = useMotionValue(COLORS_TOP[0]);
+
+  useEffect(() => {
+    animate(color, COLORS_TOP, {
+      ease: "easeInOut",
+      duration: 10,
+      repeat: Infinity,
+      repeatType: "mirror",
+    });
+  }, []);
+
+  const backgroundImage = useMotionTemplate`radial-gradient(125% 125% at 50% 0%, #020617 50%, ${color})`;
+  const border = useMotionTemplate`1px solid ${color}`;
+  const boxShadow = useMotionTemplate`0px 4px 24px ${color}`;
+
+  return (
+    <motion.section
+      style={{
+        backgroundImage,
+      }}
+      className="relative grid min-h-screen place-content-center overflow-hidden bg-zinc-950 px-4 py-24 text-zinc-200"
+    >
+      <div className="relative z-10 flex flex-col items-center">
+        <h1 className="max-w-3xl bg-gradient-to-br from-zinc-200 to-zinc-400 bg-clip-text text-center text-3xl font-semibold leading-none text-transparent sm:text-5xl sm:leading-tight md:text-7xl md:leading-tight">
+          Web design for conversions
+        </h1>
+        <p className="my-6 max-w-xl text-center text-base leading-relaxed md:text-lg md:leading-relaxed">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, et,
+          distinctio eum impedit nihil ipsum modi.
+        </p>
+        <motion.button
+          style={{
+            border,
+            boxShadow,
+          }}
+          whileHover={{
+            scale: 1.015,
+          }}
+          whileTap={{
+            scale: 0.985,
+          }}
+          className="group relative flex w-fit items-center gap-1.5 rounded-full bg-zinc-950/10 px-4 py-2 text-zinc-50 transition-colors hover:bg-zinc-950/50"
+        >
+          Start free trial
+          <FiArrowRight className="transition-transform group-hover:-rotate-45 group-active:-rotate-12" />
+        </motion.button>
+      </div>
+      <SoftwareStack />
+
+      <div className="absolute inset-0 z-0">
+        <Canvas>
+          <Stars radius={50} count={2500} factor={4} fade speed={2} />
+        </Canvas>
+      </div>
+    </motion.section>
+  );
+};
