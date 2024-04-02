e6f33ffc9c5494d309f7f09c0b06dab18de51b69
Charlie Lamb <charlielamb@Charlies-Laptop.local>
2024-04-02 13:45:10 +0100

    changed ui

--- src/components/hero/Aurora.tsx
+++ src/components/hero/Aurora.tsx
@@ -37,30 +37,58 @@ export const Aurora = () => {
       }}
       className="relative grid min-h-screen place-content-center overflow-hidden bg-zinc-950 px-4 py-24 text-zinc-200"
     >
-      <div className="relative z-10 flex flex-col items-center">
-        <h1 className="max-w-3xl bg-gradient-to-br from-zinc-200 to-zinc-400 bg-clip-text text-center text-3xl font-semibold leading-none text-transparent sm:text-5xl sm:leading-tight md:text-7xl md:leading-tight">
-          Web design for conversions
+      <div className="relative z-10 flex flex-col items-center gap-8">
+        <h1 className="bg-gradient-to-br from-zinc-200 to-zinc-400 bg-clip-text text-center text-4xl font-black leading-none text-transparent sm:text-6xl sm:leading-tight md:text-7xl md:leading-tight">
+          Charlie Lamb
         </h1>
-        <p className="my-6 max-w-xl text-center text-base leading-relaxed md:text-lg md:leading-relaxed">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, et,
-          distinctio eum impedit nihil ipsum modi.
-        </p>
-        <motion.button
-          style={{
-            border,
-            boxShadow,
-          }}
-          whileHover={{
-            scale: 1.015,
-          }}
-          whileTap={{
-            scale: 0.985,
-          }}
-          className="group relative flex w-fit items-center gap-1.5 rounded-full bg-zinc-950/10 px-4 py-2 text-zinc-50 transition-colors hover:bg-zinc-950/50"
-        >
-          Start free trial
-          <FiArrowRight className="transition-transform group-hover:-rotate-45 group-active:-rotate-12" />
-        </motion.button>
+        <div className="grid max-w-[75%] grid-cols-1 items-center gap-8 lg:grid-cols-3">
+          <div className="flex w-full flex-col gap-2 lg:gap-4">
+            <h2 className="bg-gradient-to-br from-zinc-200 to-zinc-400 bg-clip-text text-center text-2xl font-black leading-none text-transparent sm:text-3xl sm:leading-tight md:text-4xl md:leading-tight">
+              Landing Pages
+            </h2>
+            <p className="text-center text-zinc-300">
+              Landing pages that convert with Nextjs, taking the customer from
+              your landing page to checkout and minimizing clicks in the
+              process.
+            </p>
+          </div>
+          <div className="flex w-full flex-col gap-2 lg:gap-4">
+            <h2 className="bg-gradient-to-br from-zinc-200 to-zinc-400 bg-clip-text text-center text-2xl font-black leading-none text-transparent sm:text-3xl sm:leading-tight md:text-4xl md:leading-tight">
+              Web Applications
+            </h2>
+            <p className="text-center text-zinc-300">
+              Complex web applications with Nextjs capable of handling large
+              user bases and heavy requests.
+            </p>
+          </div>
+          <div className="flex w-full flex-col gap-2 lg:gap-4">
+            <h2 className="bg-gradient-to-br from-zinc-200 to-zinc-400 bg-clip-text text-center text-2xl font-black leading-none text-transparent sm:text-3xl sm:leading-tight md:text-4xl md:leading-tight">
+              Mobile Development
+            </h2>
+            <p className="text-center text-zinc-300">
+              Modern mobile applications with React Native that are fast,
+              responsive, and regularly updated
+            </p>
+          </div>
+        </div>
+        <a href="mailto:charlie@charlielamb.com">
+          <motion.button
+            style={{
+              border,
+              boxShadow,
+            }}
+            whileHover={{
+              scale: 1.015,
+            }}
+            whileTap={{
+              scale: 0.985,
+            }}
+            className="group relative flex w-fit items-center gap-1.5 rounded-full bg-zinc-950/10 px-4 py-2 text-zinc-50 transition-colors hover:bg-zinc-950/50"
+          >
+            Get In Touch
+            <FiArrowRight className="transition-transform group-hover:-rotate-45 group-active:-rotate-12" />
+          </motion.button>
+        </a>
       </div>
       <SoftwareStack />
 
